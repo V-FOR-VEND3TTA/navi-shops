1fa75703629800f08e14141584dabd5d937fdb91
George Hlongwane <georgehlongwane8@gmail.com>
2023-12-13 00:08:28 +0200

    Added our nearbyshops app and configured it in our settings

--- shops/settings.py
+++ shops/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis', # for gis to work properly
+    # custom installed apps
+    'nearbyshops',
 ]
 
 MIDDLEWARE = [
