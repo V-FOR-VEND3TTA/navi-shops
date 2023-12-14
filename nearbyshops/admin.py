940ee34ffe5d566b210e9834224e83055f6d5c9a
George Hlongwane <georgehlongwane8@gmail.com>
2023-12-14 08:06:24 +0200

    Add shop table to Django admin

--- nearbyshops/admin.py
+++ nearbyshops/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from djnago.contrib.gis.admin import OSMGeoAdmin
+from .models import Shop
+
 
 # Register your models here.
+@admin.register(Shop)
+class ShopAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location')
